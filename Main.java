002da88865362ec94062f55b9d834459690d4efb
samyside <samyside@yandex.ru>
2022-07-22 00:31:28 +0300

    Accept 3 parameters only. Exceptions for some situations. Methods for operations. Stub-methods for parsing.

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter splited by spaces : ");
+		System.out.print("enter : ");
 		String input = sc.nextLine();
 		sc.close();
 
@@ -12,16 +12,85 @@ class Main {
 		System.out.println(result);
 	}
 
-	public static String calc(String input) {
+	public static String calc(String input) throws Exception {
+		String result = null;
 		String[] items = input.split(" ");
 		// Проверить кол-во чисел для арифметики
+		if (items.length > 3) throw new Exception("Too many parameters");
+		int a = 0, b = 0;
+		
 		// Числа арабские или римские
-		// Преобразовать римские в арабские
-		// Арифметическая операция над числами
-		// Преобразовать в римские (при необходимости)
-		int a = Integer.valueOf(items[0]);
-		int b = Integer.valueOf(items[1]);
+		boolean numberRoman = false;
+		if (numberRoman = isRomanNumber("", "")) {
+			// Преобразовать римские в арабские
+			a = parseRomanToArabic(items[0]);
+			b = parseRomanToArabic(items[2]);
+		} else {
+			a = Integer.valueOf(items[0]);
+			b = Integer.valueOf(items[2]);
+		}
 
-		return String.valueOf(a+b);
+		// Арифметическая операция над числами
+		String operation = items[1];
+		switch (operation) {
+			case "+": add(a, b); break;
+			case "-": minus(a, b); break;
+			case "*": multiply(a, b); break;
+			case "/": division(a, b); break;
+			default: throw new Exception("Unable operation");
+		}
+
+		// Преобразовать в римские (при необходимости)
+		if (numberRoman) {
+			result = parseArabicToRoman();
+		}
+
+		return result;
+	}
+
+	static int parseRomanToArabic(String number) {
+		return 0;
+	}
+
+	static String parseArabicToRoman() {
+
+		return null;
+	}
+
+	static boolean isRomanNumber(String a, String b) {
+
+		return false;
+	}
+
+	static int add(int a, int b) throws Exception {
+		int result = a + b;
+		if (result > 3999) {
+			throw new Exception("Result is too much");
+		}
+		return result;
+	}
+
+	static int minus(int a, int b) throws Exception {
+		int result = a - b;
+		if (result < 0) {
+			throw new Exception("Result is too few");
+		}
+		return result;
+	}
+
+	static int multiply(int a, int b) throws Exception {
+		int result = a * b;
+		if (result > 3999) {
+			throw new Exception("Result is too much");
+		}
+		return result;
+	}
+
+	static int division(int a, int b) throws Exception {
+		int result = a / b;
+		if (a % b > 0) {
+			throw new Exception("Result should be Integer");
+		}
+		return result;
 	}
 }
